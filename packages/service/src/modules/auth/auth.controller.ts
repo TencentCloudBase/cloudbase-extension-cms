6548fd6723d39c9354f3e18120e8641dac5d0ca3
chhpt <cwuyiqing@gmail.com>
2020-08-09 00:15:56 +0800

    update(auth) update project structure and auth module

--- packages/service/src/modules/auth/auth.controller.ts
+++ packages/service/src/modules/auth/auth.controller.ts
@@ -1,74 +1,75 @@
-import { Controller, Post, Body, Inject, HttpException, HttpStatus } from '@nestjs/common'
-import { CloudBase } from '@cloudbase/node-sdk/lib/cloudbase'
-
-import { genPassword, getEnvIdString } from '@/utils'
-import config from '@/config'
+import { Controller, Post, Body } from '@nestjs/common'
+import { genPassword, getFullDate } from '@/utils'
+import { CloudBaseService } from '@/dynamic_modules'
+import { CollectionV2 } from '@/constants'
 
 @Controller('auth')
 export class AuthController {
-    // login by username and password
+    constructor(private readonly cloudbaseService: CloudBaseService) {}
+
     @Post('login')
     async login(@Body() body) {
-        const { userName, password } = body
+        const { username, password } = body
 
-        // if (!userName || !password) {
-        //   return {
-        //     message: '用户名或者密码不能为空',
-        //     code: 'LOGIN_WRONG_INPUT'
-        //   }
-        // }
-
-        return {
-            code: 'ok',
+        if (!username || !password) {
+            return {
+                message: '用户名或者密码不能为空',
+                code: 'LOGIN_WRONG_INPUT',
+            }
         }
 
-        // // 查询用户信息
-        // const collection = this.app.database().collection(config.collection.users)
-        // const query = collection.where({
-        //   userName
-        // })
-        // const getRes = await query.get()
-        // const dbRecord = getRes.data[0]
+        // 查询用户信息
+        const collection = this.cloudbaseService.collection(CollectionV2.Users)
 
-        // if (!dbRecord) {
-        //   return {
-        //     message: '用户名或者密码错误',
-        //     code: 'LOGIN_WRONG_INPUT'
-        //   }
-        // }
+        const query = collection.where({
+            username,
+        })
 
-        // const { password: dbPassword, createTime, role, failedLogins } = dbRecord
-        // const now = new Date()
-        // const todayDate = `${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()}`
+        const getRes = await query.get()
+        const dbRecord = getRes.data[0]
 
-        // if (failedLogins?.[todayDate] >= 5) {
-        //   return {
-        //     message: '登录失败次数过多，请明日再试',
-        //     code: 'LOGIN_RETRY_TOO_MANY'
-        //   }
-        // }
+        if (!dbRecord) {
+            return {
+                message: '用户名或者密码错误',
+                code: 'LOGIN_WRONG_INPUT',
+            }
+        }
 
-        // // 对密码加密
-        // const envId = getEnvIdString()
-        // console.log('环境 Id', envId)
-        // const salt = createTime + envId
-        // console.log(salt)
-        // const genPasswordResult = await genPassword(password, salt)
+        const { password: dbPassword, createTime, failedLogins } = dbRecord
 
-        // console.log(genPasswordResult, dbPassword)
+        const todayDate = getFullDate()
 
-        // if (genPasswordResult !== dbPassword) {
-        //   await collection.doc(dbRecord._id).update({
-        //     failedLogins: {
-        //       [todayDate]: failedLogins ? failedLogins[todayDate] + 1 : 1
-        //     }
-        //   })
+        if (failedLogins?.[todayDate] >= 5) {
+            return {
+                message: '登录失败次数过多，请明日再试',
+                code: 'LOGIN_RETRY_TOO_MANY',
+            }
+        }
 
-        //   return {
-        //     message: '用户名或者密码错误',
-        //     code: 'LOGIN_WRONG_INPUT'
-        //   }
-        // }
+        const genPasswordResult = await genPassword(password, createTime)
+
+        // 密码错误
+        if (genPasswordResult !== dbPassword) {
+            await collection.doc(dbRecord._id).update({
+                failedLogins: {
+                    [todayDate]: failedLogins ? failedLogins[todayDate] + 1 : 1,
+                },
+            })
+
+            return {
+                message: '用户名或者密码错误',
+                code: 'LOGIN_WRONG_INPUT',
+            }
+        }
+
+        // 生成 ticket，使用用户的 id
+        const ticket = this.cloudbaseService.auth().createTicket(dbRecord._id, {
+            refresh: 3600 * 1000,
+        })
+
+        return {
+            ticket,
+        }
     }
 
     @Post('currentUser')
