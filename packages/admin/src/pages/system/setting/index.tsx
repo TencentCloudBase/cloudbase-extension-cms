96c909d6d665103fa9a8ec99a1b69836d06fc837
chhpt <cwuyiqing@gmail.com>
2020-08-12 00:11:58 +0800

    [update] udpate system user management

--- packages/admin/src/pages/system/setting/index.tsx
+++ packages/admin/src/pages/system/setting/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
-import { Row, Col, Divider, Space, Typography } from 'antd'
 import { history } from 'umi'
+import ProCard from '@ant-design/pro-card'
 import { LeftCircleTwoTone } from '@ant-design/icons'
+import { Tabs, Row, Col, Space, Typography } from 'antd'
 import UserManagement from './UserManagement'
+import PolicyManagement from './PolicyManagement'
 import styles from './index.less'
+
+const { TabPane } = Tabs
 
 export default (): React.ReactNode => {
     return (
         <Row className={styles.settings}>
             <Col flex="1 1 auto" />
-            <Col flex="0 0 800px">
+            <Col flex="0 0 900px">
                 <div className={styles.back} onClick={() => history.push('/home')}>
                     <Space align="center" style={{ marginBottom: '10px' }}>
                         <LeftCircleTwoTone style={{ fontSize: '20px' }} />
@@ -17,8 +21,16 @@ export default (): React.ReactNode => {
                     </Space>
                 </div>
                 <Typography.Title level={3}>系统设置</Typography.Title>
-                <Divider />
-                <UserManagement />
+                <ProCard>
+                    <Tabs tabBarStyle={{ padding: '0 24px' }}>
+                        <TabPane tab="用户" key="1">
+                            <UserManagement />
+                        </TabPane>
+                        <TabPane tab="策略" key="2">
+                            <PolicyManagement />
+                        </TabPane>
+                    </Tabs>
+                </ProCard>
             </Col>
             <Col flex="1 1 auto" />
         </Row>
