f759db70301da378eef565747c695c3d4da4de73
Booker Zhao <bookerzhao@tencent.com>
2020-08-15 11:19:33 +0800

    build(scripts): fix build scripts

--- packages/tcb-ext-cms-init/scripts/deploy.js
+++ packages/tcb-ext-cms-init/scripts/deploy.js
@@ -2,7 +2,6 @@ const fs = require('fs')
 const util = require('util')
 const path = require('path')
 const writeFile = util.promisify(fs.writeFile)
-const exec = util.promisify(require('child_process').exec)
 
 module.exports = {
   // 部署静态资源
@@ -10,14 +9,9 @@ module.exports = {
     const { config, deployPath, manager } = context
     const filterDeployPath = deployPath.replace(/^\//, '')
     const { envId } = config
-    try {
-      await exec('cp -r build /tmp')
-    } catch (e) {}
 
     // 写入静态网站配置
     await writeConfigJSON(manager, envId, filterDeployPath)
-    // 同步静态网站
-    return deployHostingFile(manager, '/tmp/build', filterDeployPath)
   }
 }
 
