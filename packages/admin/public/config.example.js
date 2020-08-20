fcd76491a13561565b8cf9900dde912a3065689e
cwuyiqing <cwuyiqing@gmail.com>
2020-08-20 09:42:04 +0800

    [update] 更新部署说明文档

--- packages/admin/public/config.example.js
+++ packages/admin/public/config.example.js
@@ -3,7 +3,7 @@ window.TcbCmsConfig = {
     history: 'hash',
     // 环境 Id
     envId: 'Your EnvId',
-    // 云接入默认域名/自定义域名，不带 https 协议符
+    // 云接入默认域名/自定义域名 + 云接入路径，不带 https 协议符
     // https://console.cloud.tencent.com/tcb/env/access
     cloudAccessPath: 'xxx-xxx.service.tcloudbase.com/tcb-ext-cms-service',
 }
