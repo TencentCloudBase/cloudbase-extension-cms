d5c42a05a303f960e7d447c2c1d015bbaf7a1598
cwuyiqing <cwuyiqing@gmail.com>
2020-08-20 15:29:52 +0800

    [fix] 修复部署脚本开启密码登录失败的问题 & 优化登录失败提示信息

--- packages/admin/public/config.x.js
+++
@@ -1,8 +0,0 @@
-window.TcbCmsConfig = {
-    history: 'hash',
-    // 环境 Id
-    envId: 'cloudcms',
-    // 云接入默认域名/自定义域名，不带 https 协议符
-    // https://console.cloud.tencent.com/tcb/env/access
-    cloudAccessPath: 'cloudcms.service.tcloudbase.com/tcb-ext-cms-service',
-}
