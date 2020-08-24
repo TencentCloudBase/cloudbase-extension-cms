8b956df8af448e04329682610d2cb554f247b082
cwuyiqing <cwuyiqing@gmail.com>
2020-08-24 20:37:05 +0800

    update: 更换富文本编辑器, 支持富文本上传图片

--- packages/admin/src/pages/project/content/components/RichText.tsx
+++ packages/admin/src/pages/project/content/components/RichText.tsx
@@ -1,57 +1,114 @@
+import { Upload, Progress, message, Modal } from 'antd'
 import React, { useEffect, useState } from 'react'
-import { Spin } from 'antd'
-import loaderScript from './loader'
+import { InboxOutlined, FileImageTwoTone } from '@ant-design/icons'
+import { ContentUtils } from 'braft-utils'
+import BraftEditor, { ExtendControlType } from 'braft-editor'
+import { uploadFile, getTempFileURL } from '@/utils'
+import 'braft-editor/dist/index.css'
+
+const { Dragger } = Upload
 
 const RichText: React.FC<{ value?: any; key: string; onChange?: (...args: any) => void }> = (
   props
 ) => {
   const { key = 'default', value = '欢迎使用富文本编辑器', onChange = (...args: any) => {} } = props
-
-  const [loading, setLoading] = useState(false)
+  const [editorState, setEditorState] = useState<any>()
 
   useEffect(() => {
-    setLoading(true)
-    loaderScript('https://cdn.bootcdn.net/ajax/libs/tinymce/5.4.1/tinymce.min.js', (err) => {
-      console.log(err, window.tinymce)
-      if (err || !window.tinymce) {
-        return
-      }
-
-      setLoading(false)
-
-      window.tinymce.init({
-        selector: `#${key}-richtext-editor`,
-        height: 500,
-        menubar: true,
-        toolbar: `undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help`,
-        init_instance_callback: (editor: any) => {
-          console.log('init')
-          if (value) {
-            editor.setContent(value)
-          }
-
-          editor.on('NodeChange Change KeyUp SetContent', () => {
-            const content = editor.getContent()
-            onChange(content)
-          })
-        },
-      })
-    })
-    return () => {
-      try {
-        const tinymce = window.tinymce.get(`${key}-richtext-editor`)
-        console.log(tinymce)
-        tinymce?.destroy()
-      } catch (e) {
-        // ignore error
-      }
-    }
+    const state = BraftEditor.createEditorState(value)
+    setEditorState(state)
   }, [])
+
+  const extendControls: ExtendControlType[] = [
+    {
+      key: 'antd-uploader',
+      type: 'component',
+      component: (
+        <CustomUploader
+          onChange={(url) => {
+            const state = ContentUtils.insertMedias(editorState, [
+              {
+                type: 'IMAGE',
+                url,
+              },
+            ])
+            setEditorState(state)
+          }}
+        />
+      ),
+    },
+  ]
+
+  return (
+    <div style={{ border: '1px solid #d1d1d1' }}>
+      <BraftEditor
+        key={key}
+        value={editorState}
+        onChange={(s) => {
+          setEditorState(s)
+          onChange(s.toHTML())
+        }}
+        excludeControls={['media']}
+        extendControls={extendControls}
+      />
+    </div>
+  )
+}
+
+export const CustomUploader: React.FC<{
+  onChange?: (v: string) => void
+}> = (props) => {
+  let { onChange = () => {} } = props
+  const [fileList, setFileList] = useState<any[]>()
+  const [percent, setPercent] = useState(0)
+  const [uploading, setUploading] = useState(false)
+  const [visible, setVisible] = useState(false)
 
   return (
     <>
-      {loading && <Spin />}
-      <div id={`${key}-richtext-editor`} />
+      <button type="button" data-title="媒体上传" className="control-item button">
+        <FileImageTwoTone className="bfi-list" onClick={() => setVisible(true)} />
+      </button>
+      <Modal closable={true} visible={visible} footer={null} onCancel={() => setVisible(false)}>
+        <Dragger
+          fileList={fileList}
+          listType="picture"
+          beforeUpload={async (file) => {
+            setUploading(true)
+            setPercent(0)
+            // 上传文件
+            let fileId
+            try {
+              fileId = await uploadFile(file, (percent) => {
+                setPercent(percent)
+              })
+            } catch (error) {
+              console.log(error)
+              message.error(`上传文件失败：${error.message}`)
+              return
+            }
+            const url = await getTempFileURL(fileId)
+
+            onChange(url)
+            setFileList([
+              {
+                uid: fileId,
+                name: file.name,
+                status: 'done',
+              },
+            ])
+            message.success(`上传图片成功`)
+            setVisible(false)
+            return Promise.reject()
+          }}
+        >
+          <p className="ant-upload-drag-icon">
+            <InboxOutlined />
+          </p>
+          <p className="ant-upload-text">点击或拖拽图片上传</p>
+        </Dragger>
+        {uploading && <Progress style={{ paddingTop: '10px' }} percent={percent} />}
+      </Modal>
     </>
   )
 }
