f2432259d0b89dcaac297484080170fed311f6ec
cwuyiqing <cwuyiqing@gmail.com>
2021-01-08 16:25:51 +0800

    feat: install sentry only in production mode

--- packages/admin/config/platform.ts
+++ packages/admin/config/platform.ts
@@ -8,6 +8,8 @@ SERVER_MODE && console.log('容器服务模式构建')
 
 const name = WX_MP ? '内容管理（CMS）' : 'CloudBase CMS'
 
+const { REACT_APP_ENV } = process.env
+
 /**
  * 和平台（小程序 OR 腾讯云）相关的一些配置
  */
@@ -17,6 +19,7 @@ const platformConfig: IConfig = {
     WX_MP,
     SERVER_MODE,
     CMS_TITLE: name,
+    ENV: REACT_APP_ENV,
     ICON_PATH: WX_MP ? 'icon-wx.svg' : 'icon.svg',
   },
   layout: {
