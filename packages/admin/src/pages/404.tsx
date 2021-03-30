47ae4d1e492db1e96b4ca42c898aaf6896becaab
chhpt <cwuyiqing@gmail.com>
2021-03-30 11:35:29 +0800

    feat: 404 redirect refactor

--- packages/admin/src/pages/404.tsx
+++ packages/admin/src/pages/404.tsx
@@ -1,18 +1,38 @@
 import { Button, Result } from 'antd'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { history } from 'umi'
 
-const NoFoundPage: React.FC<{}> = () => (
-  <Result
-    status="404"
-    title="404"
-    subTitle="你访问的页面不存在"
-    extra={
-      <Button type="primary" onClick={() => history.push('/home')}>
-        回到首页
-      </Button>
+const NoFoundPage: React.FC<{}> = () => {
+  const [count, setCount] = useState(5)
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCount((count) => count - 1)
+    }, 1000)
+
+    return () => {
+      clearInterval(timer)
     }
-  />
-)
+  }, [])
+
+  useEffect(() => {
+    if (count <= 0) {
+      history.push('/home')
+    }
+  }, [count])
+
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle={`你访问的页面不存在，${count} 秒后自动返回首页`}
+      extra={
+        <Button type="primary" onClick={() => history.push('/home')}>
+          回到首页
+        </Button>
+      }
+    />
+  )
+}
 
 export default NoFoundPage
