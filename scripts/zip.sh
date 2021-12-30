eaf3bbfc8b49d41c4bbb007804d93681b92967e5
Mrjing <837317210@qq.com>
2021-12-30 17:30:54 +0800

    fix: fix

--- scripts/zip.sh
+++ scripts/zip.sh
@@ -29,7 +29,7 @@ zipFunction() {
   DEST_FILE="$__ABS_PATH__/build/$1.zip"
 
   rm -rf $DEST_FILE
-  zip -r $DEST_FILE . -x 'node_modules/*' -x '.DS_Store' -x 'src/*' -x yarn.lock -x .env.local
+  zip -r $DEST_FILE . -x 'node_modules/*' -x '.DS_Store' -x 'src/*' -x yarn.lock -x .env.local -x .env.wx.local
   cd -
 }
 
